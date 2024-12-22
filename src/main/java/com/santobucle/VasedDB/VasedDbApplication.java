89a91bd38dd284ce8f036c99bb6f83fd6aaf271b
Christian G <christiangv99@gmail.com>
2024-12-22 21:57:31 +0000

    Initial commit

---
+++ src/main/java/com/santobucle/VasedDB/VasedDbApplication.java
@@ -0,0 +1,13 @@
+package com.santobucle.VasedDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VasedDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VasedDbApplication.class, args);
+	}
+
+}
