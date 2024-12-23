94062f77bf7a055cdbfb02fd42ca0508e85a48c4
Christian G <christiangv99@gmail.com>
2024-12-23 01:32:07 +0000

    Add Stage entity

---
+++ src/main/java/com/santobucle/VaseDB/entity/Stage.java
@@ -0,0 +1,27 @@
+package com.santobucle.VaseDB.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "stage")
+public class Stage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long stage_id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+}
