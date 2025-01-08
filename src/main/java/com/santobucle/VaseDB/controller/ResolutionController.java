5df917e81af86e7ad324daf99f26d31078f94274
Christian G <christiangv99@gmail.com>
2025-01-08 23:37:25 +0000

    Add controllers

---
+++ src/main/java/com/santobucle/VaseDB/controller/ResolutionController.java
@@ -0,0 +1,32 @@
+package com.santobucle.VaseDB.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.santobucle.VaseDB.dto.ResolutionDto;
+import com.santobucle.VaseDB.service.ResolutionService;
+
+import lombok.AllArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/resolutions")
+public class ResolutionController {
+    
+    @Autowired
+    private ResolutionService resolutionService;
+
+    @PostMapping
+    public ResponseEntity<ResolutionDto> createResolution(@RequestBody ResolutionDto resolutionDto) {
+        ResolutionDto savedResolution = resolutionService.createResolution(resolutionDto);
+        return new ResponseEntity<>(savedResolution, HttpStatus.CREATED);
+    }
+    
+}
--- src/main/java/com/santobucle/VaseDB/controller/StageController.java
+++ src/main/java/com/santobucle/VaseDB/controller/StageController.java
@@ -1,5 +1,6 @@
 package com.santobucle.VaseDB.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,15 +12,30 @@ import com.santobucle.VaseDB.service.StageService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/stages")
 public class StageController {
 
+    @Autowired
     private StageService stageService;
     
+    @GetMapping("{stageId}")
+    public ResponseEntity<StageDto> getStageById(@PathVariable Long stageId) {
+        StageDto stageDto = stageService.getStageById(stageId);
+        return ResponseEntity.ok(stageDto);
+    }
+
+    @GetMapping("{stageName}")
+    public ResponseEntity<StageDto> getStageById(@PathVariable String stageName) {
+        StageDto stageDto = stageService.getStageByName(stageName);
+        return ResponseEntity.ok(stageDto);
+    }
+
     @PostMapping
     public ResponseEntity<StageDto> createStage(@RequestBody StageDto stageDto) {
         StageDto savedStage = stageService.createStage(stageDto);
