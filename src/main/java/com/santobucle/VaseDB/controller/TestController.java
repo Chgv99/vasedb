483058b8b6ce25a1297906a98e5f4bd35070f37d
Christian G <christiangv99@gmail.com>
2025-06-09 23:52:12 +0100

    Improve test endpoint

--- src/main/java/com/santobucle/VaseDB/controller/TestController.java
+++ src/main/java/com/santobucle/VaseDB/controller/TestController.java
@@ -1,15 +1,21 @@
 package com.santobucle.VaseDB.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/api/test")
 public class TestController {
-    
+
     @GetMapping
-    public String user() {
-        return "user";
+    public ResponseEntity<String> test(HttpServletRequest request) {
+        System.out.println(">>> /test endpoint hit from: " + request.getRemoteAddr());
+        return ResponseEntity.ok()
+                .header("X-API", "SpringBoot")
+                .body("API is reachable");
     }
 }
