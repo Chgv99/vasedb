74cbe79902ffca06e498f5fe9d02a9cebb2592f8
Christian G <christiangv99@gmail.com>
2025-06-11 19:02:47 +0100

    Add test endpoint with no authentication

--- src/main/java/com/santobucle/VaseDB/controller/TestController.java
+++ src/main/java/com/santobucle/VaseDB/controller/TestController.java
@@ -2,17 +2,23 @@ package com.santobucle.VaseDB.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
-@RequestMapping("/api/test")
 public class TestController {
 
-    @GetMapping
+    @GetMapping("/test")
     public ResponseEntity<String> test(HttpServletRequest request) {
+        System.out.println(">>> /test endpoint hit from: " + request.getRemoteAddr());
+        return ResponseEntity.ok()
+                .header("X-API", "SpringBoot")
+                .body("API is reachable");
+    }
+
+    @GetMapping("/api/test")
+    public ResponseEntity<String> testAuth(HttpServletRequest request) {
         System.out.println(">>> /test endpoint hit from: " + request.getRemoteAddr());
         return ResponseEntity.ok()
                 .header("X-API", "SpringBoot")
