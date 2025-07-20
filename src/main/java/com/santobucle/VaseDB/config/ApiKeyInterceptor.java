ba2280af149424cab4930e7c9e75d6911fe6adff
Christian G <christiangv99@gmail.com>
2025-07-20 18:35:20 +0100

    Disable apikeyinterceptor

--- src/main/java/com/santobucle/VaseDB/config/ApiKeyInterceptor.java
+++ src/main/java/com/santobucle/VaseDB/config/ApiKeyInterceptor.java
@@ -1,28 +1,28 @@
-package com.santobucle.VaseDB.config;
+// package com.santobucle.VaseDB.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerInterceptor;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.stereotype.Component;
+// import org.springframework.web.servlet.HandlerInterceptor;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+// import jakarta.servlet.http.HttpServletRequest;
+// import jakarta.servlet.http.HttpServletResponse;
 
-@Component
-public class ApiKeyInterceptor implements HandlerInterceptor {
+// @Component
+// public class ApiKeyInterceptor implements HandlerInterceptor {
 
-    private final String apiKey;
+//     private final String apiKey;
 
-    public ApiKeyInterceptor(@Value("${vasedb.api.key}") String apiKey) {
-        this.apiKey = apiKey;
-    }
+//     public ApiKeyInterceptor(@Value("${vasedb.api.key}") String apiKey) {
+//         this.apiKey = apiKey;
+//     }
 
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        String headerApiKey = request.getHeader("X-API-KEY");
-        if (apiKey.equals(headerApiKey)) {
-            return true;
-        }
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        return false;
-    }
-}
+//     // @Override
+//     // public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//     //     String headerApiKey = request.getHeader("X-API-KEY");
+//     //     if (apiKey.equals(headerApiKey)) {
+//     //         return true;
+//     //     }
+//     //     response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+//     //     return false;
+//     // }
+// }
